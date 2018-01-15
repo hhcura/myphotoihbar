d9be0aeb93f613cc8d1ebe493d3f21cb8529b73f
edogan25 <aedgn25@gmail.com>
2018-01-15 17:44:46 +0300

    emrah yeni ihbar

--- src/pages/yeni-ihbar/yeni-ihbar.ts
+++ src/pages/yeni-ihbar/yeni-ihbar.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 /**
  * Generated class for the YeniIhbarPage page.
@@ -15,11 +16,69 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class YeniIhbarPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public alerCtrl: AlertController) {
   }
-
+//public alerCtrl: AlertController
+//public navCtrl: NavController, public navParams: NavParams 
   ionViewDidLoad() {
     console.log('ionViewDidLoad YeniIhbarPage');
   }
+  testRadioOpen: boolean;
+  testRadioResult;
 
+
+  doRadio() {
+    let alert = this.alerCtrl.create();
+    alert.setTitle('İhbar Türü');
+
+    alert.addInput({
+      type: 'radio',
+      label: 'Yanlış Park',
+      value: 'yanlıspark',
+      checked: true
+    });
+
+    alert.addInput({
+      type: 'radio',
+      label: 'Kazı Çalışması',
+      value: 'kazıcalısması'
+    });
+
+    alert.addInput({
+      type: 'radio',
+      label: 'Hatalı Tabela',
+      value: 'hatalıtabela'
+    });
+
+    alert.addInput({
+      type: 'radio',
+      label: 'Işık İhlali',
+      value: 'isikihlali'
+    });
+
+    alert.addInput({
+      type: 'radio',
+      label: 'Orman Yangını',
+      value: 'ormanyangını'
+    });
+
+    alert.addInput({
+      type: 'radio',
+      label: 'Diğer',
+      value: 'diger'
+    });
+    alert.addButton('İptal');
+    alert.addButton({
+      text: 'Tamam',
+      handler: data => {
+        console.log('Radio data:', data);
+        this.testRadioOpen = false;
+        this.testRadioResult = data;
+      }
+    });
+
+    alert.present().then(() => {
+      this.testRadioOpen = true;
+    });
+}
 }
